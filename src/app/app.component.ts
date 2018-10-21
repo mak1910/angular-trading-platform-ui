d061d431aa25e0dd8ea7aa482114057150e02ec0
Mridul Kothari <mak1910@outlook.com>
2018-10-21 10:08:59 +0530

    Add user model and pass title to header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Client } from './models/client';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'vadapav-trading-platform';
+
+export class AppComponent implements OnInit {
+  
+  title:string;
+  client: Client;
+
+  constructor() {
+    this.title = 'Trading Platform';
+    this.client = new Client();
+  }
+
+  ngOnInit(): void {}
+  
 }
---
+++ src/app/models/client.ts
@@ -0,0 +1,6 @@
+
+export class Client {
+  id: number;
+  name: string; 
+  email: string;
+}
